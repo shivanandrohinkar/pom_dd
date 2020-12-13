98bf154bf76b6315636e4843b13d049737b5fc49
tusharthere <tusharthere@gmail.com>
2020-12-13 19:25:17 +0530

    [tushar] new feature added

---
+++ src/test/java/sample/Maven_practice1/AppTest.java
@@ -0,0 +1,12 @@
+package sample.Maven_practice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
